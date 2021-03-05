2965b9c709eeb5740bf60224c7eec2cbfb04af2c
iikim <iikim2511@gmail.com>
2021-03-05 10:45:50 -0500

    Initialize project using Create React App

---
+++ personal reference/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//🦄🦄c2 자바스크립트의 목적과 셀렉터
+document.write("🦄🦄🦄🦄c2 자바스크립트의 목적과 셀렉터");
+console.log("🦄🦄🦄🦄c2 자바스크립트의 목적과 셀렉터");
+
+//  C 58 . JS 58
+
+// @ Selectors
+
+// @ Event Listeners
+
+// @ Funcitons
+function name(event) {
+  // todo DIV
+  // Create LI
+}
+
+
