5615d24a872720e3ca8586f244454d6861c89892
iikim <iikim2511@gmail.com>
2021-03-05 17:07:48 -0500

    const GlobalStyle = createGlobalStyle`

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+/* eslint-disable */
+
+{/* library */}
+import React from 'react'
+import Game from './components/Game'
+
+
+import { createGlobalStyle } from "styled-components";
+
+
+ {/* components */}
+
+ const GlobalStyle = createGlobalStyle`
+  * {
+    margin:0;
+    padding: 0;
+    box-sizing:border-box;    
+  }
+`;
+
 
 function App() {
   return (
     <div className="App">
-      react
+    main page
+
+    <GlobalStyle></GlobalStyle>
+    <Game></Game>
+      
     </div>
   );
 }
