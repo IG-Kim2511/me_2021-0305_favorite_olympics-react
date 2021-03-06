7b2e95fca5f642100b172d2cfdb587b0de751b38
iikim <iikim2511@gmail.com>
2021-03-05 19:45:43 -0500

    click and choose / it doesn't work

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,9 +7,7 @@ import ramen from '../img/ramen.jpg'
 import pasta from '../img/pasta.jpg'
 import fried_rice from '../img/fried-rice.jpg'
 
-
 //B:  requeire is not working.... <img className="food-img" src={require("../img/burger.jpg")}></img>
-
 
 const items =[
     {
@@ -37,18 +35,43 @@ function Game() {
     const [Foods, setFoods] = useState([]);
     const [Display, setDisplay] = useState([]);
 
+    const [Winners, setWinners] = useState([]);   {/* 3) */}
+
     useEffect(() => {
         /*  2번째 방법 : {}, return빼고 사용        
         items.sort(()=>Math.random()-0.5); */
 
         items.sort(()=>{return Math.random()-0.5});
-        // setFoods(items);
+
+        setFoods(items);    {/* 3) */}
+
         setDisplay([items[0],items[1]]); 
     }, [])
 
+    {/* 3) click event : */}
+
+    const clickHandler =(a) =>{        {/* ? */}   {/* 결승전 */}
+        if (Foods.length<2) {
+            if (Winners.length===0) {
+                setDisplay([a]);
+                
+            }else{
+                let updateFood = [...Winners,a];
+                setFoods(updateFood);
+                setDisplay([updateFood[0],updateFood[1]]);
+                setWinners([]);
+            }
+            
+        } else if(Foods.length>2) {         {/* 결승전 이전까지... 3개이상 아이템인때의 코드 */}
+            setWinners([...Winners,Foods])
+            setDisplay([Foods[2],Foods[3]])
+            setFoods(Foods.slice(2))
+        }
+    };
+
     return (
         <div>
-            <FlexBox>    
+            <FlexBox>
                 <h1 className="title">favorite olymphics</h1>
 
                 {/*  2)  */}
@@ -56,7 +79,7 @@ function Game() {
                     Display.map((a)=>{
                         return(
                           <div className="flex-1"
-                          onClick={()=>{alert('hello')}}>
+                          onClick={()=>{clickHandler(a)}}>          {/* 3) */}
                             <img className="food-img" src={a.src}></img>   
                             <div className="name">{a.name}</div>     
                           </div>
@@ -74,7 +97,7 @@ function Game() {
                     <div className="name">ramen</div>                
                 </div>
              */}
-            </FlexBox>                    
+            </FlexBox>
         </div>
     )
 }
